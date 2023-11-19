640fe7f95b893553c03374cab9d296d85580cb9f
Vicky-15 <vigneshstudent98@gmail.com>
2023-11-19 19:26:45 +0530

    infor about not finish backend

--- src/pages/AllNotes.tsx
+++ src/pages/AllNotes.tsx
@@ -6,7 +6,28 @@ const AllNotes = () => {
   return (
     <>
       <Stack divider={<Divider />} spacing={3}>
-        <PageHeading>All Notes</PageHeading>
+        <PageHeading>
+          All Notes <br />
+          <br />
+          <span style={{ color: "RED" }}>
+            {" "}
+            INFORMATION &nbsp;&nbsp; &nbsp;{" "}
+            <strong style={{ color: "yellow" }}>
+              IN SHORT TIME I COULDNOT ABLE TO FINISH THIS PROJECT(BACKEND)
+              ANYWAY, I NOW KNOW MY POTENTIAL OF BUILDING THINGS IN TIME, I WILL
+              CONTINUE THIS PROJECT. IM ABLE TO LEARN BACKEND QUICKLY IF HAVE
+              GIVEN A CHANCE
+            </strong>
+            <br />
+            TO WHOEVER SEEING THIS, I BUILT THIS FROM SCRACTH WITH REFERING
+            GOOGLE KEEP, IT TAKES TWO DAYS FOR ME, I HAVE EXPERIENCE IN BACKEND
+            TO BE ABLE TO UNDERSTAND AND BUILD ENDPOINT WITH SECURITY MEASURES,
+            I STARTED LEARNING BACKED IN THREE WEEKS AGO ONLY,
+            <br />
+            to see what i am capable of in backed please REFER, in my github
+            page, (guvi-backend)
+          </span>
+        </PageHeading>
         <NotesGrid />
       </Stack>
     </>

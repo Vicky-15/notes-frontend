6880d7dc0bbf4b353c5379e1a9914671ecbda28b
Vicky-15 <vigneshstudent98@gmail.com>
2023-11-19 16:23:00 +0530

    oraganize entities

---
+++ src/entities.ts
@@ -0,0 +1,28 @@
+import { IconType } from "react-icons";
+
+export interface Tag {
+  id: number;
+  name: string;
+}
+
+export interface Category {
+  id: number;
+  name: string;
+  notesCount: number;
+}
+
+export interface Note {
+  id: number;
+  title: string;
+  content: string;
+  tags: Tag[];
+  categoryId: Category;
+}
+
+export interface SideBarItemType {
+  id?: number;
+  name: string;
+  icon: IconType;
+  isActive: boolean;
+  gotoLink: string;
+}

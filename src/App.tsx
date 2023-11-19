a414031d6c73039e6288c8f791b0b749d5a2643a
Vicky-15 <vigneshstudent98@gmail.com>
2023-11-19 16:29:10 +0530

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,12 @@
-import { Grid, GridItem, Show } from "@chakra-ui/react";
 import "./App.css";
-import NavBar from "./components/NavBar";
-import SideBar from "./components/SideBar";
-import NotesGrid from "./components/NotesGrid";
+import AuthProvider from "./hoc/AuthProvider";
+import Routes from "./routes";
 
 function App() {
   return (
-    <Grid
-      templateAreas={{
-        base: `"nav" "main"`,
-        lg: `"nav nav" "aside main"`,
-      }}
-      templateColumns={{
-        base: "1fr",
-        lg: "250px 1fr",
-      }}
-    >
-      <GridItem area="nav">
-        <NavBar />
-      </GridItem>
-
-      <Show above="lg">
-        <GridItem area="aside" paddingX="10px">
-          <SideBar />
-        </GridItem>
-      </Show>
-
-      <GridItem area="main">
-        <NotesGrid />
-      </GridItem>
-    </Grid>
+    <AuthProvider>
+      <Routes />
+    </AuthProvider>
   );
 }
 

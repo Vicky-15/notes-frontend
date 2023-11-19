43720aa532269f99703127fadd9e83c012d8e05e
Vicky-15 <vigneshstudent98@gmail.com>
2023-11-19 16:28:06 +0530

    refactor : extracted create button

--- src/components/NotesGrid.tsx
+++ src/components/NotesGrid.tsx
@@ -1,30 +1,11 @@
-import { Button, SimpleGrid, Stack, useDisclosure } from "@chakra-ui/react";
+import { SimpleGrid, useDisclosure } from "@chakra-ui/react";
 import NoteCard from "./NoteCard";
 import ViewNoteModal from "./ViewNoteModal";
-import AddNewNoteModal from "./AddNewNoteModal";
 
 const NotesGrid = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const {
-    isOpen: isCreateModalOpen,
-    onOpen: createModalonOpen,
-    onClose: createModalonClose,
-  } = useDisclosure();
-
   return (
-    <Stack padding="10px" spacing={5}>
-      <Button
-        onClick={createModalonOpen}
-        colorScheme="gray"
-        size="lg"
-        width={"fit-content"}
-      >
-        Create Note
-      </Button>
-      <AddNewNoteModal
-        isOpen={isCreateModalOpen}
-        onClose={createModalonClose}
-      />
+    <>
       <ViewNoteModal isOpen={isOpen} onClose={onClose} />
       <SimpleGrid spacing={5} columns={{ base: 1, sm: 2, md: 3, xl: 4 }}>
         <NoteCard onOpenModal={onOpen} />
@@ -34,7 +15,7 @@ const NotesGrid = () => {
         <NoteCard onOpenModal={onOpen} />
         <NoteCard onOpenModal={onOpen} />
       </SimpleGrid>
-    </Stack>
+    </>
   );
 };
 
